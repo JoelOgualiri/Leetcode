2f55e3e753154412dd560153b29db0216ddc507a
JoelOgualiri <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-15 00:39:42 +0100

    Time: 105 ms (42.87%), Space: 44.7 MB (14.25%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -3,18 +3,11 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-    for (let i = 0; i < intervals.length; i++){
-        for (let j = 0; j < intervals.length; j++){
-                if(intervals[i][0] > intervals[j][0]){
-                    if (intervals[j][1] > intervals[i][0]) return false
-                }else if (intervals[j][0]< intervals[i][0]){
-                    if (intervals[j][1]> intervals[i][0]) return false
-                }else if ((intervals[j][0] === intervals[i][0] || intervals[j][1] === intervals[i][1]) && i !== j){
-                    return false
-                }
-            
-        }
-        
+    intervals.sort((a,b) => a[0]-b[0])
+    for (let i = 0; i < intervals.length-1; i++){
+        if (intervals[i][1] > intervals[i+1][0]) return false
     }
+    
     return true
+    
 };
