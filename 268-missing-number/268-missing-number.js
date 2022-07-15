23f23f23714b6b319395b78dd897dc0643e1584a
JoelOgualiri <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-15 12:33:37 +0100

    Time: 81 ms (82.35%), Space: 44.6 MB (33.35%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    
-    nums.sort((a,b)=>(a-b))
-    if (nums[0]!== 0) return 0
-    if (nums[nums.length-1]!== nums.length) return nums.length
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i+1] !== nums[i]+1){
-            return nums[i]+1
-        }
+    let sum = 0
+    for (let i = 1; i <= nums.length; i++){
+        sum = sum + i
     }
+    
+    for (let num of nums){
+        sum = sum - num
+    }
+    return sum
+
 };
