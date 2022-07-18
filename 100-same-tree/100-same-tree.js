04ddf504616bd66380ccb6f870998c7ba5194c8f
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-19 00:43:33 +0100

    Time: 94 ms (35.79%), Space: 42 MB (89.06%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,44 +12,9 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    let stack1 = [p]
-    let stack2 = [q]
-    while (stack1.length > 0 && stack2.length > 0){
-        let current1 = stack1.pop();
-        let current2 = stack2.pop();
-        if (current1 && current2){
-            if (current1.val !== current2.val) return false
-            if (current1.right){
-               stack1.push(current1.right);
-            }else{
-                stack1.push(null);
-            }
-            if (current1.left){
-                stack1.push(current1.left);
-            }
-            else{
-                stack1.push(null);
-            }
-             if (current2.right){
-               stack2.push(current2.right);
-            }else{
-                stack2.push(null);
-            }
-            if (current2.left){
-                stack2.push(current2.left);
-            }
-            else{
-                stack2.push(null);
-            }
-        }
-        else if (current1 === null && current2 === null){
-            continue
-        }
-        else{
-            return false
-        }
-    }
-    if (stack1.length > 0 || stack2.length > 0) return false
-    return true
+    if (p === null && q === null) return true
+    if (p === null || q === null) return false
     
+    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right,q.right)
+   
 };
