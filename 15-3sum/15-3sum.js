7087c4de84d2783f7759d32f2bf869c24d973885
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-18 14:15:04 +0100

    Time: 235 ms (46.60%), Space: 52.5 MB (72.22%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,37 +3,40 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-   let result = [];
-    
-    nums.sort((a, b) => a - b); // O(logN)
-    
-    for(let i = 0; i < nums.length - 1; i++) {
-        // can't be the same as the previous value
-        if( i > 0 && nums[i] === nums[i -1]) {
+    nums.sort((a,b)=>a-b)
+    const result = []
+    for (let i = 0; i < nums.length-1; i++){
+            if( i > 0 && nums[i] === nums[i -1]) {
             continue;
         }
-        
-        let left = i + 1;
-        let right = nums.length - 1;
-        
-        while(left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
-            
-            if(sum < 0) {
-                left++;
-            } else if(sum > 0) {
-                right--;
-            } else {
-                result.push([nums[i], nums[left], nums[right]]);
-                
-                left++;
-                //keep looking for the next one that is not equal the previous and lower than right
-                while(nums[left] === nums[left - 1] && left < right) {
-                    left++;
-                }    
+       let res = threeSumHelper(nums,i, left = i+1, right = nums.length-1)
+      
+           result.push(...res)
+       
+    }
+    return result
+};
+function threeSumHelper(nums,i, left, right){
+    let tempArr = []
+    let sum = 0
+    while ( right > left){
+        sum = nums[i] + nums[left] + nums[right]
+        if (sum === 0){
+            tempArr.push([nums[i], nums[left], nums[right]])
+            left++
+            right--  
+            while (nums[left] === nums[left-1] && left < right){
+                left++
             }
+         
+        }
+        else if (sum < 0){
+            left++
+            
+        }
+        else if (sum > 0){
+            right--
         }
     }
-    
-    return result;
+    return tempArr
 }
