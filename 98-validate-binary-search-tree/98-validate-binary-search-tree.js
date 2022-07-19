347e0a4c86725b44d4b97616c0d937d9655b2296
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-19 10:32:05 +0100

    Time: 62 ms (99.37%), Space: 46.2 MB (68.55%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+   
+    return isValidBSTHelper(root, min = -Infinity, max = Infinity)
+};
+
+function isValidBSTHelper(root, min, max){
+    if (root === null) return true
+    if (root.val <= min || root.val >= max) return false
+    
+    return  isValidBSTHelper(root.left, min, root.val) && isValidBSTHelper(root.right, root.val, max)
+}
