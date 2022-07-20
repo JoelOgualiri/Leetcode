cc5420d367e54a5355105830cd0b6936f5f121f4
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-20 13:44:48 +0100

    Time: 149 ms (57.56%), Space: 119.7 MB (38.70%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -11,6 +11,7 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
+
 var buildTree = function(preorder, inorder) {
     if (preorder.length === 0 || inorder.length === 0) return null
     const root = new TreeNode(preorder[0])
