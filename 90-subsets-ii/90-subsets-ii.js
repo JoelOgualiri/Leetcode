0f0c812c7337cfc8baa0921d503a86022f0b9f61
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-07-28 17:01:44 +0100

    Time: 73 ms (92.18%), Space: 44.4 MB (72.95%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.js
+++ 90-subsets-ii/90-subsets-ii.js
@@ -3,31 +3,24 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-       const hashmap = {};
-    nums.sort((a,b) => (a-b))
-  const res = dfs((i = 0), nums, (result = []), (slate = []));
-
-  for (let val of res) {
-    if (hashmap[val]) continue;
-    hashmap[val] = val;
+    nums.sort((a,b) => a-b)
+  const hm = {};
+  const result = subsetsWithDupHelper(nums);
+  for (let nums of result) {
+    hm[nums] = nums;
   }
-  return Object.values(hashmap);
-};
 
-function dfs(i, nums, result, slate) {
-  if (i === nums.length) {
-    
-      result.push(slate.slice());
-    
-    return;
-  }
-  //exclude
-
-  dfs(i + 1, nums, result, slate);
-  //include
-  slate.push(nums[i]);
-  dfs(i + 1, nums, result, slate);
-  slate.pop();
-
-  return result;
+  return Object.values(hm);
 }
+
+function subsetsWithDupHelper(array) {
+  if (array.length === 0) return [[]];
+  const first = array[0];
+  const includeNum = [];
+  const subset = subsetsWithDupHelper(array.slice(1));
+  for (let num of subset) {
+    includeNum.push([...num, first]);
+  }
+  return [...includeNum, ...subset];
+}
+
