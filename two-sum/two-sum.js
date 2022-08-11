477e078a987c4eaa6eac78dd0c216d5b62bcea9e
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-08-11 20:11:36 +0100

    Time: 92 ms, Memory: 43 MB - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hm = {}
+    for (let i = 0; i < nums.length; i++){
+        
+        if (nums[i] in hm) return [hm[nums[i]], i]
+        hm[target-nums[i]] = i
+    }
+
+};
