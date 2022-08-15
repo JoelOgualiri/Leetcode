4a3ffc7bb810b6d77ce0142d69763b2be591be7b
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-08-15 18:49:51 +0100

    Time: 76 ms (69.55%), Space: 41.9 MB (95.12%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,9 +12,10 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (p === null && q === null) return true
+    if (p === null && q=== null) return true
     if (p === null || q === null) return false
+    if ( p.val !== q.val) return false
     
-    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right,q.right)
+    return isSameTree(p.left,q.left) && isSameTree(p.right, q.right)
    
 };
