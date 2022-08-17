689326d70715449cb773a10372cfb8f46735c92b
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-08-17 05:30:18 +0100

    Time: 99 ms (68.55%), Space: 45.3 MB (36.09%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -3,16 +3,22 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
+    
+    
+    return permuatationHelper(nums)
+};
+
+function permuatationHelper(nums){
     if (nums.length === 0) return [[]]
     
-    const first = nums[0]
     const result = []
-    const permutation = permute(nums.slice(1))
+    const first = nums[0]
+    const permutations = permuatationHelper(nums.slice(1))
     
-    for (let perm of permutation){
-        for (let i = 0; i <= perm.length;i++){
-            result.push([...perm.slice(0,i), first, ...perm.slice(i)])
+    for (let num of permutations){
+        for (let i = 0; i <= num.length; i++){
+            result.push([...num.slice(0,i), first, ...num.slice(i)])
         }
     }
     return result
-};
+}
