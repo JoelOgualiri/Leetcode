d4d648ed520a4de10ceac425b310e733ee3bcf45
Joel <46266471+JoelOgualiri@users.noreply.github.com>
2022-08-21 01:08:34 +0100

    Time: 112 ms (87.87%), Space: 49.2 MB (25.05%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -10,79 +10,44 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-    const dummy = new ListNode()
-    let tail = dummy
-    const heapNodes = new MinHeap
-    for (let list of lists){
-        let current = list
-        while (current !== null){
-            heapNodes.insert(current)
-            current = current.next
-        }
-    }
+   
+    if (lists.length === 0) return null
+    const result = mergeLists(lists)
     
-    while (true){
-        tail.next = heapNodes.remove()
-        if (tail.next === null){
-            break
-        }
-        tail = tail.next
-        
-    }
-    return dummy.next
+    return result[0]
+
 }
 
-class MinHeap{
-    constructor(){
-        this.values = [null]
-    }
-    swap(index1,index2){
-        return [this.values[index1],this.values[index2]] = [this.values[index2],this.values[index1]]
-    }
-    insert(node){
-        this.values.push(node)
-        if (this.values.length > 2){
-            let childIdx = this.values.length - 1
-            while (childIdx > 1 && this.values[Math.floor(childIdx/2)].val > this.values[childIdx].val){
-                this.swap(Math.floor(childIdx/2), childIdx)
-                childIdx = Math.floor(childIdx/2)
-            }
+function mergeLists(lists){
+        if (lists.length === 1) return lists
+
+    let mid = Math.floor(lists.length/2)
+    let leftList = lists.slice(0,mid)
+    let rightList = lists.slice(mid)
+
+    return merge(mergeLists(leftList), mergeLists(rightList))
+}
+
+function merge(leftList, rightList){
+    const dummyHead = new ListNode()
+    let tail = dummyHead;
+    let left = leftList[0];
+    let right = rightList[0];
+    while (left !== null && right !== null){
+        if (left.val < right.val){
+            tail.next = left
+            left = left.next
+        }else{
+            tail.next = right
+            right = right.next
         }
+        tail = tail.next
     }
-    
-    remove(){
-        if (this.values.length === 1) return null
-        const min = this.values[1]
-        let end = this.values.pop()
-        if (this.values.length === 1) return min
-        this.values[1] = end
-        if (this.values.length === 2){
-            return min
-        }
-        if (this.values.length === 3){
-            if (this.values[1].val > this.values[2].val){
-                this.swap(1,2)
-            }
-            return min
-        }
-        let parent = 1;
-        let leftChild = 2 * parent;
-        let rightChild = 2*parent + 1;
-        
-        while (this.values[parent].val > this.values[leftChild].val || this.values[parent].val > this.values[rightChild].val){
-            if (this.values[leftChild].val < this.values[rightChild].val){
-                this.swap(leftChild,parent)
-                parent = leftChild
-            }else{
-                this.swap(rightChild, parent)
-                parent = rightChild
-            }
-            leftChild = 2*parent
-            rightChild = 2 * parent +1
-            
-            if (this.values[leftChild] === undefined || this.values[rightChild] === undefined)
-                return min
-        }
-        return min
+    if (left === null){
+        tail.next = right
     }
+    if (right === null){
+        tail.next = left
+    }
+    return [dummyHead.next]
 }
